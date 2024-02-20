8285564b862ac7967bf8353560b8035577eb4b18
ummarFurquan <mohammedummar29@gmail.com>
2024-02-20 18:35:07 +0530

    Update experienceData

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Ernst and Youngs (QUEST), Washington D.C.',
+        jobtitle: 'Statistical Staff Analyst',
+        startYear: 'Jun 2022 ',
+        endYear: ' Current'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Mercer; Marh & Mclennan Group,Mumbai',
+        jobtitle: 'Assistant Manager (WAS)',
+        startYear: '2019 ',
+        endYear: ' 2021'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Jardine Lloyd Thompson, Mumbai',
+        jobtitle: 'Acturial Analyst',
+        startYear: 'Mar-2018 ',
+        endYear: ' Sep-2019'
     },
 ]
